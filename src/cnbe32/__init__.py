ac5ee321c4ec5b2e94f479788690966578f404b7
zairkliu <526019501@qq.com>
2026-07-06 19:34:45 +0800

    Fix hamming_distance export in cnbe32/__init__.py

--- src/cnbe32/__init__.py
+++ src/cnbe32/__init__.py
@@ -1,8 +1,9 @@
-"""CNBE-32: Chinese Native Binary Encoding - Python SDK"""
-from .core import CNBE32, encode_cnbe, decode_cnbe
+﻿"""CNBE-32: Chinese Native Binary Encoding - Python SDK"""
+from .core import CNBE32, encode_cnbe, decode_cnbe, hamming_distance
 from .skill_table import SkillTable
 from .encoders import (
     TreeEncoder, TyphoonEncoder, BlackHoleEncoder,
     SocialEncoder, MathEncoder, RawEncoder, OneHotEncoder, RandomEncoder
 )
 __version__ = "0.2.0"
+
